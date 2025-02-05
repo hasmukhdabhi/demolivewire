685e20f4e1bd673324437835d41ea0a593e44b02
hasmukhdabhi <skey.hasmukh@gmail.com>
2025-02-05 12:27:14 +0530

    profile component

---
+++ app/Livewire/Profile.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Profile extends Component
+{
+    public function render()
+    {
+        $data = [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'phone' => '123-456-7890',
+        ];
+        return view('livewire.profile', ['data' => $data]);
+    }
+}
---
+++ resources/views/livewire/profile.blade.php
@@ -0,0 +1,6 @@
+<div>
+    {{-- Be like water. --}}
+    @foreach ($data as $item)
+        <h1>{{ $item }}</h1>
+    @endforeach
+</div>
