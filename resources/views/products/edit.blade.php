5126ea6e05f55dc93eda8ace59c4e26f0e264a85
hasmukhdabhi <skey.hasmukh@gmail.com>
2025-02-19 10:35:18 +0530

    new crud products

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,54 @@
+@extends('products.layout')
+
+@section('content')
+    <div class="card mt-5">
+        <h2 class="card-header">Edit Product</h2>
+        <div class="card-body">
+
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <a class="btn btn-primary btn-sm" href="{{ route('products.index') }}"><i class="fa fa-arrow-left"></i>
+                    Back</a>
+            </div>
+
+            <form action="{{ route('products.update', $product->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+
+                <div class="mb-3">
+                    <label for="inputName" class="form-label"><strong>Name:</strong></label>
+                    <input type="text" name="name" value="{{ $product->name }}"
+                        class="form-control @error('name') is-invalid @enderror" id="inputName" placeholder="Name">
+                    @error('name')
+                        <div class="form-text text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="inputPrice" class="form-label"><strong>Price:</strong></label>
+                    <input type="text" name="price" value="{{ $product->price }}"
+                        class="form-control @error('price') is-invalid @enderror" id="inputPrice" placeholder="Price">
+                    @error('price')
+                        <div class="form-text text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="inputCategory" class="form-label"><strong>Category:</strong></label>
+                    <input type="text" name="category" value="{{ $product->Category }}"
+                        class="form-control @error('category') is-invalid @enderror" id="inputCategory" placeholder="Category">
+                    @error('category')
+                        <div class="form-text text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="inputDetail" class="form-label"><strong>Detail:</strong></label>
+                    <textarea class="form-control @error('detail') is-invalid @enderror" style="height:150px" name="detail"
+                        id="inputDetail" placeholder="Detail">{{ $product->detail }}</textarea>
+                    @error('detail')
+                        <div class="form-text text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-success"><i class="fa-solid fa-floppy-disk"></i> Update</button>
+            </form>
+
+        </div>
+    </div>
+@endsection
