8f3d9039ba4ad8506bc20a6135f53d3baedcc08b
hasmukhdabhi <skey.hasmukh@gmail.com>
2025-02-21 10:25:38 +0530

    login register route

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,12 @@ use App\Livewire\ProductSave;
 // use App\Http\Livewire\ProductComponent;
 use App\Livewire\CrudForm;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Auth;
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 
 
 /*
@@ -37,30 +42,33 @@ Route::get('/posts/create', CreatePost::class);
 Route::view('search', 'usersearch');
 Route::view('user', 'user');
 
-// Route::view('product', 'livewire.product-save');
-// Route::get('/add-product', ProductSave::class);
-
-// profile route
-
-// Route::view('/profile', 'profile');
-
-
-// Route::get('/products', ProductComponent::class);
-// Route::get('/products', ProductComponent::class)->name('products');
-
-// Route::get('/', CrudForm::class);
-
 Route::get('/products', function () {
     return view('products');
 });
 // Route::get('/products', CrudForm::class);
 Route::resource('products', ProductController::class);
-// Route::resource('posts', ProductController::class);
-Route::get("posts", [ProductController::class, "index"])->name("posts.index");
-Route::get("posts/create", [ProductController::class, "create"])->name("posts.create");
-Route::post("posts", [ProductController::class, "store"])->name("posts.store");
-Route::get("posts/{post}", [ProductController::class, "show"])->name("posts.show");
-Route::get("posts/{post}/edit", [ProductController::class, "edit"])->name("posts.edit");
-Route::put("posts/{post}", [ProductController::class, "update"])->name("posts.update");
-Route::delete("posts/{post}", [ProductController::class, "destroy"])->name("posts.destroy");
 
+// login register route
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::get('/password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('/password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('/password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('/password/reset', [ResetPasswordController::class, 'reset']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
